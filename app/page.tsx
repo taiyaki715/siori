dc0f0c9e472f847d09e275a1019f25969db2d1c2
taiki-ishikawa <taiyaki715@icloud.com>
2024-10-12 21:40:32 +0900

    サインアウト実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
-import { signIn } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <form
       action={async () => {
         "use server"
-        await signIn("google")
+        session ? await signOut() : await signIn("google")
       }}
     >
-      <button type="submit">Signin with Google</button>
+      <button type="submit">
+        { session ? "Sign out" : "Sign in with Google" }
+      </button>
     </form>
   );
 }
